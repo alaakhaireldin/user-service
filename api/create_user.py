ef2f9ff7d0effaec495ef6bc8152500d1f5eebf3
Ehab Khaireldin <ehabkhaireldin@gmail.com>
2022-07-04 16:31:59 +0200

    configuring create_user function

--- api/create_user.py
+++ api/create_user.py
@@ -3,20 +3,32 @@ import logging
 from lib import todo_service
 
 def handler(event, context):
-  body = json.loads(event['body'])
+  print(event)
 
-  if 'name' not in body:
+  body = event['body']
+
+  if body is None:
+    error_description = "Must include a body with the request"
+    logging.error(error_description)
+    return { "statusCode": 400, "body": json.dumps({ "error": error_description }) }
+
+  parsedBody = json.loads(body)
+
+  if 'name' not in parsedBody:
     error_description = "Validation failed, the name is not in the request body"
     logging.error(error_description)
-    raise Exception(error_description)
+    return { "statusCode": 400, "body": json.dumps({ "error": error_description }) }
 
-  if 'age' not in body:
+  if 'age' not in parsedBody:
     error_description = "Validation failed, the age is not in the request body"
     logging.error(error_description)
-    raise Exception(error_description)
+    return { "statusCode": 400, "body": json.dumps({ "error": error_description }) }
   
-  todo_service.create_user_in_db(name, age)
+  name = parsedBody["name"]
+  age = parsedBody["age"]
 
-  return { "statusCode": 200, "body": { "created": True } }
+  user_id = todo_service.create_user_in_db(name, age)
+
+  return { "statusCode": 200, "body": json.dumps({ "userId": user_id }) }
 
   
--- lib/todo_service.py
+++ lib/todo_service.py
@@ -1,3 +1,4 @@
+import os
 import uuid
 from lib import db
 
@@ -6,7 +7,7 @@ db_client = db.dynamodb_client
 USERS_TABLE = os.environ['USERS_TABLE']
 
 def generate_user_id():
-  return uuid.uuid4()
+  return str(uuid.uuid4())
 
 def get_user_from_db(user_id):
   result = db_client.get_item(
@@ -17,5 +18,6 @@ def get_user_from_db(user_id):
 
 def create_user_in_db(name, age):
   user_id = generate_user_id()
-  item = { 'userId': {'S': user_id }, 'name': { 'S': name }, 'age': { 'N': age } }
+  item = { 'userId': {'S': user_id }, 'name': { 'S': name }, 'age': { 'N': str(age) } }
   db_client.put_item(TableName=USERS_TABLE, Item=item)
+  return user_id
