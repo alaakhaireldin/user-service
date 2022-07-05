403a44a7c7a32e1b7c57ea4329d9d6a8fe2e8fb5
alaakhaireldin <alaa.khaireldin@gmail.com>
2022-07-05 18:37:08 +0200

    adding testing requirements

--- tests/test_error.py
+++ tests/test_error.py
@@ -4,5 +4,5 @@ import json
 
 def test_error_function():
     message = "things didn't work"
-    expected_results = { "statusCode": 400, "body": json.dumps({ "error": message }) }
+    expected_results = { "statusCode": 404, "body": json.dumps({ "error": message }) }
     assert get_http_error(400, message) == expected_results
